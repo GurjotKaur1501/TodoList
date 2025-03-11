5a539b1251b2390d4c343a7537a2c1da76d4ac13
GurjotKaur1501 <jotkau0115@gmail.com>
2025-03-11 13:49:06 +0100

    Adding Todolist.java

---
+++ src/main/java/ToDoList.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ToDoList {
+    private List<Task> tasks = new ArrayList<>();
+
+    public void addTask(String title) {
+        tasks.add(new Task(title));
+    }
+
+    public void removeTask(String title) {
+        tasks.removeIf(task -> task.getTitle().equals(title));
+    }
+
+    public List<Task> getTasks() {
+        return tasks;
+    }
+}
