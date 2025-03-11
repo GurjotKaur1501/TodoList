67cf252e0fa16906080b423d05f3326cff27b0bd
GurjotKaur1501 <jotkau0115@gmail.com>
2025-03-11 14:59:38 +0100

    Adding ToDoListTest.java

--- src/test/java/se/yrgo/ToDoListTest.java
+++ src/test/java/se/yrgo/ToDoListTest.java
@@ -1,18 +1,17 @@
 package se.yrgo;
 
-import ;
-import org.junit.Test;
+import se.yrgo.ToDoList;
 import java.util.List;
-import static org.junit.Assert.*;
+import org.testng.annotations.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TodoListTest {
     @Test
     public void testAddTask() {
-        TodoList todoList = new TodoList();
+        ToDoList todoList = new ToDoList();
         todoList.addTask("Buy milk");
         List<Task> tasks = todoList.getTasks();
         assertEquals(1, tasks.size());
         assertEquals("Buy milk", tasks.get(0).getTitle());
     }
 }
-
